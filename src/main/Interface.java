d42541b226eb3fc88745893d1241165741e38e42
Jordan Stone <professoroflaserology@gmail.com>
2014-07-25 17:46:54 -0400

    First Code Commit

---
+++ src/main/Interface.java
@@ -0,0 +1,16 @@
+/*
+ * Jordan Stone
+ * stone@drexel.edu
+ * CS338:GUI, Personal Project
+ * 
+ * Interface is the active class for this project. It includes main and creates the GUI.
+ */
+
+package main;
+
+public class Interface {
+
+	public static void main(){
+		
+	}
+}
